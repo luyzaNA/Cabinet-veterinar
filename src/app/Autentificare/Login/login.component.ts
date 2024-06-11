f1561142da47a50881bafbb139482a48c4d9d571
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2024-06-11 23:15:00 +0300

    Adaugare pagini de register + login

---
+++ src/app/Autentificare/Login/login.component.ts
@@ -0,0 +1,40 @@
+import {Component, ViewChild} from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
+import {FormsModule, NgForm} from "@angular/forms";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
+import {MatInput} from "@angular/material/input";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatIcon,
+    MatLabel,
+    MatFormField,
+    FormsModule,
+    MatButton,
+    RouterLink,
+    MatIconButton,
+    MatInput,
+    MatSuffix
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  @ViewChild('loginForm') loginForm!: NgForm;
+  hide = true;
+
+  clickEvent(event: MouseEvent) {
+    this.hide = !this.hide;
+    event.stopPropagation();
+  }
+
+  onLogin(email: string, password: string) {
+    console.log(email, password);
+    alert("Logarea s a efectuat cu succes");
+    this.loginForm.resetForm();
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {RegisterComponent} from "./Autentificare/Register/register.component";
+import {LoginComponent} from "./Autentificare/Login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'register', component: RegisterComponent
+  },
+  {
+    path:'login', component: LoginComponent
+  }
+];
