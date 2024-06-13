c8e4e2fe7e2941b43a4dbb9572b44c5b4d355975
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2024-06-13 15:42:09 +0300

    Adaugare sectiune de contact

--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import {ContactComponent} from "./contact/contact.component";
 
 @Component({
   selector: 'app-first-page',
   templateUrl: './first-page.component.html',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    ContactComponent
   ],
   styleUrls: ['./first-page.component.css']
 })
