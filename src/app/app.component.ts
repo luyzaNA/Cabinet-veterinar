6d221dc8b66ded8af73ed1f337142c751751ac57
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2024-06-13 12:56:15 +0300

    Adaugare continut first-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {RegisterComponent} from "./Autentificare/Register/register.component";
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegisterComponent],
+  imports: [RouterOutlet, RegisterComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-first-page',
   templateUrl: './first-page.component.html',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   styleUrls: ['./first-page.component.css']
 })
 export class FirstPageComponent {
+
+
 
 }
